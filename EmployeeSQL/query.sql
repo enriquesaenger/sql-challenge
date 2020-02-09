ab3cb03c7590a07adb830c813c9ddc6aef5e47e3
ricksanchez <esaenger@me.com>
2020-02-09 17:54:39 -0600

    Imported CSVs and created tables

--- EmployeeSQL/query.sql
+++ EmployeeSQL/query.sql
@@ -1,1 +1,79 @@
+-- statements to drop tables as needed
+drop table departments;
+drop table dept_emp;
+drop table dept_manager;
+drop table employees;
+drop table salaries;
+drop table titles;
+
+-- create tables
+-- departments table
+create table departments(
+	dept_no VARCHAR(4) PRIMARY KEY,
+	dept_name VARCHAR(20)
+);
+-- dept_emp table
+create table dept_emp(
+	emp_no INT,
+	dept_no VARCHAR(4),
+	from_date VARCHAR(10),
+	to_date VARCHAR(10)
+);
+-- dept_manager table
+create table dept_manager(
+	dept_no VARCHAR(4),
+	emp_no INT,
+	from_date VARCHAR(10),
+	to_date VARCHAR(10)
+);
+-- employees table
+create table employees(
+	emp_no INT PRIMARY KEY,
+	birth_date VARCHAR(10),
+	first_name VARCHAR(30),
+	last_name VARCHAR(30),
+	gender VARCHAR(1),
+	hire_date VARCHAR(10)
+);
+-- salaries table
+create table salaries(
+	emp_no INT PRIMARY KEY,
+	salary INT,
+	from_date VARCHAR(10),
+	to_date VARCHAR(10)
+);
+-- titles table
+create table titles(
+	emp_no INT,
+	title VARCHAR(30),
+	from_date VARCHAR(10),
+	to_date VARCHAR(10)
+);
+
 -- import CSVs
+-- import departments CSV
+copy departments from '/Users/enriquesaenger/Bootcamp/sql-challenge/data/departments.csv'
+delimiter ',' csv header;
+-- import dept_emp CSV
+copy dept_emp from '/Users/enriquesaenger/Bootcamp/sql-challenge/data/dept_emp.csv'
+delimiter ',' csv header;
+-- import dept_manager CSV
+copy dept_manager from '/Users/enriquesaenger/Bootcamp/sql-challenge/data/dept_manager.csv'
+delimiter ',' csv header;
+-- import employees CSV
+copy employees from '/Users/enriquesaenger/Bootcamp/sql-challenge/data/employees.csv'
+delimiter ',' csv header;
+-- import salaries CSV
+copy salaries from '/Users/enriquesaenger/Bootcamp/sql-challenge/data/salaries.csv'
+delimiter ',' csv header;
+-- import titles CSV
+copy titles from '/Users/enriquesaenger/Bootcamp/sql-challenge/data/titles.csv'
+delimiter ',' csv header;
+
+-- view entire tables
+select * from departments;
+select * from dept_emp;
+select * from dept_manager;
+select * from employees;
+select * from salaries;
+select * from titles;
