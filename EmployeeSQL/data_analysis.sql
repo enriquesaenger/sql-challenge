62926ff2c85681fa2485e3d77816d89f2596cb55
ricksanchez <esaenger@me.com>
2020-02-15 19:58:05 -0600

    Cleaned up queries by adding joins

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -34,38 +34,34 @@ where "Hire Date" like '1986%';
 -- List the manager of each department with the following information:
 -- department number, department name, the manager's employee number, last name, first name
 -- and start and end employment dates.
-select m.dept_no as "Department Number", (
-	select d.dept_name
-	from departments as d
-	where d.dept_no = m.dept_no
-) as "Department Name", m.emp_no as "Employee Number", (
-	select e.last_name
-	from employees as e
-	where e.emp_no = m.emp_no
-) as "Last Name", (
-	select e.first_name
-	from employees as e
-	where e.emp_no = m.emp_no
-) as "First Name", m.from_date as "Employment Start Date", m.to_date as "Employment End Date"
-from dept_manager as m;
+select i.dept_no as "Department Number",
+		d.dept_name as "Department Name",
+		i.emp_no as "Employee Number",
+		e.last_name as "Last Name",
+		e.first_name as "First Name",
+		i.from_date as "From Date",
+		i.to_date as "To Date"
+from dept_manager as i
+inner join employees as e on e.emp_no = i.emp_no
+inner join departments as d on d.dept_no = i.dept_no;
 
 -- #4
 -- List the department of each employee with the following information:
 -- employee number, last name, first name, and department name.
-select i.emp_no as "Employee Number", (
-	select e.last_name
-	from employees as e
-	where i.emp_no = e.emp_no
-) as "Last Name", (
-	select e.first_name
-	from employees as e
-	where i.emp_no = e.emp_no
-) as "First Name", (
-	select d.dept_name
-	from departments as d
-	where d.dept_no = i.dept_no
-) as "Department Name"
-from dept_emp as i;
+-- drop view as needed
+drop view dept_info
+-- create view with needed info
+create view dept_info as
+select i.emp_no as "Employee Number",
+		e.last_name as "Last Name",
+		e.first_name as "First Name",
+		d.dept_name as "Department Name"
+from dept_emp as i
+inner join employees as e on e.emp_no = i.emp_no
+inner join departments as d on d.dept_no = i.dept_no;
+-- list the data
+select *
+from dept_info;
 
 -- #5
 -- List all employees whose first name is "Hercules" and last names begin with "B."
@@ -76,40 +72,16 @@ and last_name like 'B%';
 
 -- #6
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
-select i.emp_no as "Employee Number", (
-	select e.last_name
-	from employees as e
-	where e.emp_no = i.emp_no
-) as "Last Name", (
-	select e.first_name
-	from employees as e
-	where e.emp_no = i.emp_no
-) as "First Name", (
-	select d.dept_name
-	from departments as d
-	where d.dept_no = i.dept_no
-) as "Department Name"
-from dept_emp as i
-where i.dept_no in (
-	select d.dept_no
-	from departments as d
-	where d.dept_name = 'Sales'
-);
+select *
+from dept_info
+where "Department Name" in ('Sales');
 
 -- #7
 -- List all employees in the Sales and Development departments, including their
 -- employee number, last name, first name, and department name.
-
--- GOOD ONE vvvvvvvvvv
-select i.emp_no as "Employee Number",
-		e.last_name as "Last Name",
-		e.first_name as "First Name",
-		d.dept_name as "Department Name"
-from dept_emp as i
-inner join employees as e on e.emp_no = i.emp_no
-inner join departments as d on d.dept_no = i.dept_no
-where d.dept_name in('Sales', 'Development');
--- GOOD ONE ^^^^^^^^^^
+select *
+from dept_info
+where "Department Name" in ('Sales', 'Development');
 
 -- #8
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
